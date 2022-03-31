1e2523b1b8ec4b2fa0fe9647213f2347e7c1d79b
dibss <ludovicad01@gmail.com>
2022-03-31 17:44:31 +0200

    timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-// Partendo da un array di immagini, realizziamo uno slider con Vue.js come da mockup
-// Al click delle freccette "sinistra" e "destra" lo slider cambierà l'immagine visibile passando alla successiva oppure alla precedente.
-
-// Attenzione
-
-// Quando le immagini terminano, lo slider ricomincerà dalla prima
-// Bonus
-
-// applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente ( riguardare la documentazione su i lifecycle hooks )
 // Al click su un pallino verrà visualizzata l'immagine di riferimento associata ( il terzo pallino sarà associato alla terza immagine e così via..)
 
 var app = new Vue({
@@ -25,10 +16,6 @@ var app = new Vue({
     dots: function(){
       
     },
-    
-    timerSlide: function() {
-      this.timer = setInterval(this.next, 4000);
-    },
 
     next: function() {
       if (this.currentIndex < this.slideshowImages.length - 1){
@@ -45,5 +32,9 @@ var app = new Vue({
         this.currentIndex -= 1;
       }
     },
+  },
+
+  mounted() {
+    this.timer = setInterval(this.next, 3000);
   }
 });
